b9d50724f47084048c1f430f07a9ef5261b678f6
sadiapeu22 <afripeu2000@gmail.com>
2024-05-06 11:13:44 +0600

    encrypt decrypt added

--- index.py
+++ index.py
@@ -8,3 +8,18 @@ def caesar_encrypt(text, shift):
                 new_ascii_code = new_ascii_code - 26
             new_char = chr(new_ascii_code)
             result += new_char
+        elif char .isupper():
+            ascii_code = ord(char)
+            new_ascii_code = ascii_code + shift
+            if new_ascii_code > ord('Z')
+                new_ascii_code = new_ascii_code -26
+            new_char = chr(new_ascii_code)
+            result = result + new_char
+
+        else:
+            result = result + char
+    return result
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
